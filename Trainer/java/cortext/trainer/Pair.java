62525ff7ef4325495d4ee3c29c5c83d0cceaddbc
ed <knhjp@oldcomp-desktop.(none)>
2010-03-05 21:54:24 -0600

    added description for Pair

--- Trainer/java/cortext/trainer/Pair.java
+++ Trainer/java/cortext/trainer/Pair.java
@@ -5,7 +5,7 @@ package cortext.trainer;
  * User: knhjp
  * Date: Mar 5, 2010
  * Time: 9:52:20 PM
- * To change this template use File | Settings | File Templates.
+ * This is a simple pair observation made to update database
  */
 public interface Pair {
 }
