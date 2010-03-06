752d5668f80a6c9f269d4801288abef271cd0245
ed <knhjp@oldcomp-desktop.(none)>
2010-03-06 08:12:20 -0600

    introducing PairImpl

---
+++ Trainer/java/cortext/trainer/PairImpl.java
@@ -0,0 +1,11 @@
+package cortext.trainer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Mar 6, 2010
+ * Time: 8:12:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PairImpl {
+}
