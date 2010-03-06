0e8ba66e1a38553ce54d278c10682fe2bf9369ac
ed <knhjp@oldcomp-desktop.(none)>
2010-03-05 21:53:24 -0600

    added interface Pair

---
+++ Trainer/java/cortext/trainer/Pair.java
@@ -0,0 +1,11 @@
+package cortext.trainer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: knhjp
+ * Date: Mar 5, 2010
+ * Time: 9:52:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Pair {
+}
